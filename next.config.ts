ab006b62529634f3341981a56116a2015f9819da
TheJohny71 <jduval71@me.com>
2024-12-19 15:36:44 -0600

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
+import { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const config: NextConfig = {
+  output: 'export',
+  basePath: '/ai-dashboard',
+  images: {
+    unoptimized: true,
+  },
+}
 
-export default nextConfig;
+export default config
