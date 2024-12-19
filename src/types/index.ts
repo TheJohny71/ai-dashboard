1efc87d521bcfb37a29ff0d81fc0c517d7888820
TheJohny71 <jduval71@me.com>
2024-12-19 16:01:21 -0600

    Update index.ts

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,15 @@
-export interface DashboardConfig {
-  mainNav: MainNavItem[]
-  sidebarNav: SidebarNavItem[]
+export interface Project {
+  title: string;
+  subtitle: string;
+  description: string;
+  color: string;
+  iconSymbol: string;
+  link: string;
+  tech: string[];
 }
 
-export interface NavItem {
-  title: string
-  href: string
-  disabled?: boolean
-}
-
-export interface MainNavItem extends NavItem {}
-
-export interface SidebarNavItem extends NavItem {
-  icon?: string
-  items?: NavItem[]
+export interface StatItem {
+  label: string;
+  value: string;
+  iconSymbol: string;
 }
