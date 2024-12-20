e012cd50fe9ea2b157d19201455c1cb6f9547daa
TheJohny71 <jduval71@me.com>
2024-12-19 19:56:38 -0600

    Create types.ts

---
+++ src/app/types.ts
@@ -0,0 +1,10 @@
+export interface Solution {
+  id: string;
+  title: string;
+  subtitle: string;
+  description: string;
+  icon: any;
+  color: string;
+  impact: string;
+  practices: string[];
+}
